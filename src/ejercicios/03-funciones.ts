3bf3b06e36292e274a41a1ed96559fa67ce5f319
Daniel Hernandez <daniel244@live.com.mx>
2023-02-13 15:47:14 -0600

    Funciones

---
+++ src/ejercicios/03-funciones.ts
@@ -0,0 +1,45 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+// Funciones básicas
+
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+const sumarFlecha = (a: number, b: number): number => {
+    return a + b;
+}
+
+function multiplicar(numero: number, otroNumero?: number, base:number = 2): number {
+    return numero * base;
+}
+
+const resultado = multiplicar(5, 0, 10);
+
+console.log(resultado);
+
+// Funciones con objetos como argumentos
+
+interface PersonajeLOR {
+    nombre: string;
+    pv: number;
+    mostrarHp: () => void;
+}
+
+function curar(personaje: PersonajeLOR, curarX:number): void{
+    personaje.pv += curarX;
+}
+
+const nuevoPersonaje: PersonajeLOR = {
+    nombre: 'Strider',
+    pv: 50,
+    mostrarHp(){
+        console.log('Puntos de vida:', this.pv);
+    }
+}
+
+curar( nuevoPersonaje, 20);
+
+nuevoPersonaje.mostrarHp();
